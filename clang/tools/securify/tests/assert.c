c77222c01ffefe9936812d4cc59b0e65a045c0bb
Brice Dobry <brice.dobry@huawei.com>
2019-03-20 16:30:33 -0400

    Improve test scripts
    If called with no arguments, it will run all tests in its directory. The
    return value from the script is the return value from bats, so 0
    indicates that all tests passed (or skipped) and 1 indicates a failure.
    Also skipped assert.c for now until it is fixed.

--- clang/tools/securify/tests/assert.c
+++ clang/tools/securify/tests/assert.c
@@ -1,4 +1,5 @@
 //bats @test "assert.c: assert pointer != NULL" {
+//bats skip
 //bats run securify assert.c --
 //bats [ $status = 0 ]
 //bats [[ ${lines[8]} =~ "int * _Nonnull f" ]]
--- clang/tools/securify/tests/test.sh
+++ clang/tools/securify/tests/test.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+# Ensure securify is in the path
 which securify &> /dev/null
 if [[ $? -ne 0 ]]; then
   echo -e "\e[01;31merror: securify command not found\e[0m" >&2
   exit 1
 fi
+
+# Change to the tests directory
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# If no args are passed, test all C files in the tests directory
+testcases="$@"
+if [[ $# -eq 0 ]]; then
+  testcases=*.c
+fi
+
+# Collect the bats scripts
 OUT="$(mktemp)"
-for testcase in "$@"; do
+for testcase in $testcases; do
   grep "//bats" "$testcase" | sed 's,^//bats ,,g' >> "$OUT"
 done
+
+# Execute the tests
 bats "$OUT"
+rc=$?
+
+# Remove the temporary file
 rm "$OUT"
 
+# Return the result of running the tests
+exit $rc
