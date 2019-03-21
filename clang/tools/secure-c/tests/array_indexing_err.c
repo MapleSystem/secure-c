ebcc236b16963409a55423dfade2491d8de67ac4
Brice Dobry <brice.dobry@huawei.com>
2019-03-21 17:46:31 -0400

    Fix test cases

--- clang/tools/secure-c/tests/array_indexing_err.c
+++ clang/tools/secure-c/tests/array_indexing_err.c
@@ -1,9 +1,10 @@
 //bats @test "array_indexing_err.c: use nuallable pointer for array indexing should trigger an error" {
 //bats   run secure-c array_indexing_err.c --
 //bats   [ $status = 1 ]
-//bats   [[ ${lines[0]} =~ ":11:15: error: illegal access of nullable pointer type 'char *'" ]]
+//bats   [[ ${lines[0]} =~ ":12:15: error: illegal access of nullable pointer type 'char *'" ]]
 //bats }
 #include <stddef.h>
+#include <stdio.h>
 
 void foo() {
 	char * data;
--- clang/tools/secure-c/tests/call_error.c
+++ clang/tools/secure-c/tests/call_error.c
@@ -1,7 +1,7 @@
 //bats @test "call_error.c: Passing a nullable pointer to a non-null parameter should report an error" {
 //bats   run secure-c call_error.c --
 //bats   [ $status = 1 ]
-//bats   [[ ${lines[0]} =~ ":12:7: error: implicit conversion from nullable pointer type 'int *' to non-nullable pointer type 'int * _Nonnull'" ]]
+//bats   [[ ${lines[0]} =~ ":12:7: error: unsafe promotion from nullable pointer type 'int *' to non-nullable pointer type 'int * _Nonnull'" ]]
 //bats }
 #include <stddef.h>
 
