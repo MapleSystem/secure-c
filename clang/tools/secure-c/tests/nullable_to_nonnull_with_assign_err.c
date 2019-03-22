c7b5a3a673d2a87ff83659c34cfec9cc29807203
Brice Dobry <brice.dobry@huawei.com>
2019-03-22 15:51:07 -0400

    Fix test case (again)

--- clang/tools/secure-c/tests/nullable_to_nonnull_with_assign_err.c
+++ clang/tools/secure-c/tests/nullable_to_nonnull_with_assign_err.c
@@ -1,8 +1,8 @@
 //bats @test "nullable_to_nonnull_with_assign_err.c: make a pointer nonnull through assignment" {
 //bats   run secure-c nullable_to_nonnull_with_assign_err.c --
-//bats   [ $status = 1 ]
+//bats   [ $status = 1 ] && echo ${lines[2]}
 //bats   [[ ${lines[0]} =~ ":19:22: error: illegal access of nullable pointer type 'int *'" ]]
-//bats   [[ ${lines[1]} =~ ":21:15: error: illegal access of nullable pointer type 'int *'" ]]
+//bats   [[ ${lines[3]} =~ ":21:15: error: illegal access of nullable pointer type 'int *'" ]]
 //bats }
 
 #include <stddef.h>
