8f5ee413ba8fed1f16950209f21de385c0cd3e22
Brice Dobry <brice.dobry@huawei.com>
2019-05-03 14:15:39 -0400

    Add macros for _Nonnull and _Nullable for gcc

--- clang/tools/secure-c/secure_c.h
+++ clang/tools/secure-c/secure_c.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef __has_feature      // Optional of course.
+#define __has_feature(x) 0 // Compatibility with non-clang compilers.
+#endif
+#if !__has_feature(nullability)
+#define _Nonnull
+#define _Nullable
+#endif
+
 static void *_Nonnull _CheckNonNull(const char *_Nonnull file, int line,
                                     const char *_Nonnull func,
                                     void *_Nullable p) {
