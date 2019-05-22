567fa77f5a17ecd66c72bd90a90ac20505c36895
Brice Dobry <brice.dobry@huawei.com>
2019-05-22 11:39:14 -0400

    Remove use of known_symbols.h in secure-c
    Define the properties of the known symbols in code, rather than
    force-including the header file to avoid potential errors.

--- clang/tools/secure-c/SecureC.h
+++ clang/tools/secure-c/SecureC.h
@@ -77,6 +77,41 @@ private:
   bool isNullabilityAnnotated(const QualType &QT);
   bool hasValueRange(const FunctionDecl *FD);
   bool isNonnullCompatible(Expr const *E);
+
+  class FuncNullability {
+  public:
+    // For functions with no parameters
+    FuncNullability(NullabilityKind RK) : ReturnKind(RK) {}
+
+    // For functions with no return value
+    FuncNullability(std::initializer_list<NullabilityKind> PKs) {
+      for (NullabilityKind kind : PKs) {
+        ParamKinds.push_back(kind);
+      }
+    }
+
+    // For functions with both return value and parameters
+    FuncNullability(NullabilityKind RK,
+                    std::initializer_list<NullabilityKind> PKs)
+        : ReturnKind(RK) {
+      for (NullabilityKind kind : PKs) {
+        ParamKinds.push_back(kind);
+      }
+    }
+
+    NullabilityKind ReturnKind;
+    std::vector<NullabilityKind> ParamKinds;
+  };
+
+  std::map<StringRef, FuncNullability> KnownFuncs;
+  std::map<StringRef, NullabilityKind> KnownDecls;
+
+  void initializeKnownSymbols();
+  void initializeKnownDecls();
+  void initializeKnownFuncs();
+  bool isKnownFunction(const FunctionDecl *FD);
+  bool isNonNullParam(const FunctionDecl *FD, int i);
+  bool isKnownNonNull(const Expr *E);
 };
 
 bool isAnnotatedNonnull(const QualType &QT);
