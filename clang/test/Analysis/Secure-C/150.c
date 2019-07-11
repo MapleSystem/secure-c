a27a37f6c8fdc1c512b7ca8557043fe9f461e1ed
Brice Dobry <brice.dobry@huawei.com>
2019-07-11 15:33:56 -0400

    Update test syntax

--- clang/test/Analysis/Secure-C/150.c
+++ clang/test/Analysis/Secure-C/150.c
@@ -1,12 +1,11 @@
 // RUN: %clang -fsyntax-only -I%S/../../../tools/secure-c -Xclang -analyze \
 // RUN:   -Xclang -analyzer-config -Xclang ipa=none \
 // RUN:   -Xclang -analyzer-checker=secure-c.Nullability -Xclang -verify %s
+// expected-no-diagnostics
 #include <stddef.h>
 #include <secure_c.h>
 
-int foo(int *_Nullable p, int *_Nonnull q)
+int foo(int *p, int *q)
     __attribute__((secure_c_in(q, nonnull))) {
   return *((p != NULL) ? p : q);
 }
-
-// expected-no-diagnostics
