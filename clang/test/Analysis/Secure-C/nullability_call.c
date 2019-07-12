797b0dea047ba2bfade317d556381c767e230fde
Brice Dobry <brice.dobry@huawei.com>
2019-07-12 09:45:52 -0400

    Separate known failing tests from passing tests
    This will allow simpler automated testing and verification.

--- clang/test/Analysis/Secure-C/nullability_call.c
+++ clang/test/Analysis/Secure-C/nullability_call.c
@@ -32,29 +32,3 @@ void call(int *a, int *b) __attribute__((secure_c_in(b, nonnull))) {
   foo_nullable(e);
   foo_nonnull(e);
 }
-
-typedef struct foo {
-  int *count;
-  struct foo *ptr;
-} foo;
-
-void nonnull_field(foo *f)
-    __attribute__((secure_c_in(f, nonnull), secure_c_in(f->count, nonnull)));
-
-void call_nonnull_field(foo *f, foo *g, foo *h)
-    __attribute__((secure_c_in(g, nonnull), secure_c_in(h, nonnull),
-                   secure_c_in(h->count, nonnull))) {
-  nonnull_field(f); // expected-warning {{in-constraint is not satisfied}}
-  nonnull_field(g); // expected-warning {{in-constraint is not satisfied}}
-  nonnull_field(h);
-
-  if (f) {
-    nonnull_field(f); // expected-warning {{in-constraint is not satisfied}}
-    if (f->count)
-      nonnull_field(f);
-  }
-
-  if (g->count) {
-    nonnull_field(g);
-  }
-}
